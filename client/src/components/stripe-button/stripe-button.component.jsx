87c1524efd6d3547e118f9ea5be0c47cd8d931ec
DanielSarkwa <dannysark16@gmail.com>
2020-11-06 07:00:28 -0800

    first-commit

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment was successful');
+    }).catch(error => {
+      console.log(error);
+      // console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment. Please make sure you use the provided credit card');
+    });
+  };
+
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
